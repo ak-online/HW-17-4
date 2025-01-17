2cfff8bb486eeb5fa7ea47aa3857d06e0010e3bc
ak-online <79262567597@yandex.com>
2025-01-17 11:05:46 +0300

    Update user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -5,9 +5,9 @@ from slugify import slugify
 from sqlalchemy import insert, select, update, delete
 from sqlalchemy.orm import Session
 
-from app.backend.db_depends import get_db
-from app.models.user import User
-from app.schemas import CreateUser, UpdateUser
+from backend.db_depends import get_db
+from models.user import User
+from schemas import CreateUser, UpdateUser
 
 router = APIRouter(prefix="/user", tags=["User"])
 db_sesssion = Annotated[Session, Depends(get_db)]
