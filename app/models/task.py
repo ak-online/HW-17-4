2a775d42fb602900783e2ca47bb1ad6a43d2bdf8
ak-online <79262567597@yandex.com>
2025-01-23 18:09:28 +0300

    Update task.py

--- app/models/task.py
+++ app/models/task.py
@@ -1,12 +1,10 @@
 from app.backend.db import Base
-from sqlalchemy import Column, Integer, String, Boolean,ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.schema import CreateTable
 from app.models.user import User
 
-
 class Task(Base):
-    __tablename__ = "tasks"
+    __tablename__ = 'tasks'
     __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
@@ -15,8 +13,5 @@ class Task(Base):
     completed = Column(Boolean, default=False)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
     slug = Column(String, unique=True, index=True)
-    user = relationship("User", back_populates='tasks')
 
-
-
-print(CreateTable(Task.__table__))
+    user = relationship('User', back_populates='tasks')
