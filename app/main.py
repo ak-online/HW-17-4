06fb3e257f95d9e33ba2d8b5927433513c3330b8
ak-online <79262567597@yandex.com>
2025-01-23 18:07:33 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
-from routers import user
-from routers import task
+from app.routers import task, user
 
 app = FastAPI()
 
 @app.get("/")
 async def welcome():
-    return {"message": "Welcome to Taskmanager"}
+    return {"message": "My shop"}
 
-app.include_router(user.router)
 app.include_router(task.router)
+app.include_router(user.router)
